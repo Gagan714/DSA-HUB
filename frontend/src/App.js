4ea2691af615b3d90a0f3a9005a21c6b9234c4ef
Gagan714 <gaganursmg@gmail.com>
2025-04-18 20:00:20 +0530

    Connection between frontend and backend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{useEffect,useState} from 'react';
+function App(){
+  const[backEndMessage,setBackEndMessage]=useState("");
+ useEffect(()=>{
+  fetch("http://localhost:8080/api/test")
+  .then(Response=>Response.text())
+  .then(data=>setBackEndMessage(data))
+  .catch(error=>console.error("error loading from backend",error));
+ },[])
+ return(
+  <div>
+    <h3>DSA-HUB frontend</h3>
+    <h3>backend says</h3>
+    <h3>{backEndMessage}</h3>
+  </div>
+ )
 }
-
-export default App;
+export default App
